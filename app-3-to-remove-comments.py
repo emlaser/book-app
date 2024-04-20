376c29aa829dcb4e08f82bf199d9e23b191ec2c9
lsavoie <twosavoie@gmail.com>
2024-04-20 17:58:35 -0500

    Moved comments to a different file and cleaned up the app.py file.

--- app-3-to-remove-comments.py
+++ app-3-to-remove-comments.py
@@ -7,7 +7,14 @@ import datetime
 import re
 
 # http://127.0.0.1:5000/
-# todo Make this code more like the Skillcrush lesson...
+# todo rethink - I've learned how this works. Define the routes and run in the command line first. Then connect with the index.html. There are two routes one that POSTS the chat_history and the other (2) that GETS the threads and the chat_history. 
+# todo move the js to its own folder.
+# todo print out all the values so I'm sure what data they hold. 
+# todo Check my mapping to the JS to make sure what's sent over when. 
+# todo can the assistant still be personalized with a UI? Can it still ask a name, etc.?
+# todo clear out this code and start fresh walking through the lessons as they are. Establish a boilerplate w/flask. Maybe clear out js also. Def move later to another file. The lesson will be about understanding GET and POST in this context. The way this app works is that we GET the ... and then interact with the user and POST what they add, then we GET the thread (takes time), and then when the user asks another question... we POST to the API again. This continues until the user is done and they close the window. A nice would be that the chat is saved so they can view it later. 
+
+# Can comment out because it doesn't allow me to see the url it's running on
 log = logging.getLogger("assistant")
 
 logging.basicConfig(filename = "assistant.log", level = logging.INFO)
@@ -22,12 +29,9 @@ app = Flask(__name__)
 assistant_id = ""
 thread_id = ""
 
-# chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
-
 chat_history = [
     # todo Change this to prompt for user name, etc.
-    # When I do, the Assistant just reprints my name. Exit does work though. If I ask it to call me by my name it will. What about adding it instead of "user"? If the user writes "My name is ..." It responsds "Hello Lisa! How can I assist you today?"
-    {"role": "system", "content": "Hello there! Just so you know, you can type exit to end our chat. I'm hear to answer your Python questions. I'm happy to call you by your name. What's your name? "},
+    {"role": "system", "content": "You are a helpful assistant."},
 ]
 
 @app.route("/get_ids", methods=["GET"])
@@ -38,6 +42,8 @@ def get_ids():
 @app.route("/get_messages", methods=["GET"])
 def get_messages():
     if thread_id != "":
+        # In get_messages function, a little different though
+        # order="asc" sorts the messages in ascending order https://platform.openai.com/docs/api-reference/assistants/listAssistants#assistants-listassistants-order
         thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
         messages = [
             {
@@ -52,6 +58,7 @@ def get_messages():
         return jsonify(success=False, message="No thread ID")
   
 # todo - COMPLETED - try refactoring the function, removing the conditional to use my assistant_id
+# turning on logging worked the asssitant id printed to the command line and showed in the log
 def create_assistant():
     global assistant_id
     my_assistant = client.beta.assistants.retrieve(assistant_id = "asst_FUTO5sCQkGFaK9UAjLCGaWuq")
@@ -88,24 +95,35 @@ def chat():
         moderation_result = client.moderations.create(
             input = user_input
         )
+        # What is the capital of France?
+        # Why are fat women so ugly?
         
     chat_history.append({"role": "user", "content": user_input})
 
     # Send the message to the assistant
     message_params = {"thread_id": thread_id, "role": "user", "content": user_input}
     
+    # Within the stand alone While Loop that takes in user_input and eventually exits. Different parameters though. And this is greyed out, so is it being used? There's a thread_messages in the get_messages function
     thread_message = client.beta.threads.messages.create(**message_params)
 
+    # Run the assistant
+    # This run is within the process_run function. They way this code is written may be just fine for a UI. May not need while true since there's nothing to become false - didn't run.
     run = client.beta.threads.runs.create(
         thread_id=thread_id, assistant_id=assistant_id
     )
-
+    # Wait for the run to complete and get the response
+    # in a while loop within the process_run function (includes phrases to print out while the run is running)
+    # todo replace with 23-33 from assistant.py
     while run.status != "completed":
         time.sleep(0.5)
         run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
     
+    # within get_message function - checks if run is completed
+    # this code also has this in an additional place in the get_messages function. 
+    # It's written a bit diffferently. Here .data[0] is extracted. In ours, extracting data[0] is part of a longer line
     response = client.beta.threads.messages.list(thread_id).data[0]
 
+    # very curious about this code. Is it to evaluate if the response is only text and so return only text?
     text_content = None
 
     # Iterate through the content objects to find the first text content
@@ -116,18 +134,19 @@ def chat():
 
     # Check if text content was found
     if text_content:
+        # this is the wrong place for moderation. No need to moderate the content from the assistant
         chat_history.append({"role": "assistant", "content": text_content})
         return jsonify(success=True, message=text_content)
     else:
         # Handle the case where no text content is found
         return jsonify(success=False, message="No text content found")
 
+# Broken route? 405? So does the route in the original openai-quickstart-python assistant-flask
 @app.route("/reset", methods=["POST"])
 def reset_chat():
     global chat_history
-    # chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
     # todo change this also to the message we want to see
-    chat_history = [{"role": "system", "content": "Hello there! Just so you know, you can type exit to end our chat. What's your name? "}]
+    chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
 
     global thread_id
     thread_id = ""
