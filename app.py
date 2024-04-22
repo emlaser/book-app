b04603ace2b29817398d765dab4af17a4bc5c4cd
lsavoie <twosavoie@gmail.com>
2024-04-22 11:44:32 -0500

    Refactored POST request

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import logging
 import datetime
 import re
 
-# http://127.0.0.1:5000/
-# todo Make this code more like the Skillcrush lesson...
+# app will run at: http://127.0.0.1:5000/
+
+# set up logging in the assistant.log file
+# todo change the name of the file??
 log = logging.getLogger("assistant")
 
 logging.basicConfig(filename = "assistant.log", level = logging.INFO)
-
-# todo add the rest of logging... but where? :) 
 
 from openai import OpenAI
 
@@ -19,23 +19,21 @@ client = OpenAI()
 
 app = Flask(__name__)
 
-# Initialize the Assistant and Thread globally
+# Initialize the Assistant and Thread globally so all functions have access to the assistnt_id and thread_id
 assistant_id = ""
 thread_id = ""
 
-# chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
-
+# The array that will hold the chat history as the user and the assistant interact
 chat_history = [
-    # todo Change this to prompt for user name, etc.
-    # When I do, the Assistant just reprints my name. Exit does work though. If I ask it to call me by my name it will. What about adding it instead of "user"? If the user writes "My name is ..." It responsds "Hello Lisa! How can I assist you today?"
     {"role": "system", "content": "Hey there! How can i assist you with your learning today?"},
 ]
 
+# Start by getting the assistant_id and thread_id and returning them
 @app.route("/get_ids", methods=["GET"])
 def get_ids():
     return jsonify(assistant_id=assistant_id, thread_id=thread_id)
 
-
+# If there is a message thread, send it to the the JavaScript using msg.role to render it to the UI. # # If there isn't a thread, return an error
 @app.route("/get_messages", methods=["GET"])
 def get_messages():
     if thread_id != "":
@@ -47,24 +45,21 @@ def get_messages():
             }
             for msg in thread_messages.data
         ]
-        # todo throwing and indexerror - log out thread_messages.data. Should this be in a POST request instead?
-        # if thread_messages.data[0].content[0].text.annotations:
-        #     pattern = r'【\d+†source】'
-        #     message = re.sub(pattern, '', message)
         
         return jsonify(success=True, messages=messages)
     else:
         return jsonify(success=False, message="No thread ID")
   
-# todo - COMPLETED - try refactoring the function, removing the conditional to use my assistant_id
+# Use your assitant_id to retrieve your a assistant from the openai playground
+# Replace "asst_yournewassistantID" with your assistant ID
+# todo remove my assistant id before add, commit, push new version
 def create_assistant():
     global assistant_id
     my_assistant = client.beta.assistants.retrieve(assistant_id = "asst_FUTO5sCQkGFaK9UAjLCGaWuq")
     assistant_id = my_assistant.id
-    # print(assistant_id)
     return my_assistant
 
-
+# Create a thread if there isn't one
 def create_thread():
     global thread_id
     if thread_id == "":
@@ -76,18 +71,20 @@ def create_thread():
 
     return thread
 
+# Function to add to the log in the assistant.log file
+# todo rename file
 def log_run(run_status):
     if run_status in ["cancelled", "failed", "expired"]:
         log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
 
+# Render the HTML template - we're going to see a UI!!!
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", chat_history=chat_history)
 
-# Broken route? 405? So does the route in the original openai-quickstart-python assistant-flask
+# This is the POST route for the chat. Adds the chat to the chat_history array and sends it to the assistant playground on openai
 @app.route("/chat", methods=["POST"])
-def chat():
-    # todo - COMPLETED - added moderation to chat 
+def chat(): 
     user_input = request.json["message"]
 
     moderation_result = client.moderations.create(
@@ -116,35 +113,30 @@ def chat():
     while run.status != "completed":
         time.sleep(0.5)
         run = client.beta.threads.runs.retrieve(thread_id = thread_id, run_id = run.id)
+        
+    thread_messages = client.beta.threads.messages.list(thread_id)
+    
+    message = thread_messages.data[0].content[0].text.value
+
+    if thread_messages.data[0].content[0].text.annotations:
+        pattern = r'【\d+†source】'
+        message = re.sub(pattern, '', message)
+          
+    if run.status in ["cancelled", "failed", "expired"]:
+        message = "An error has occurred, please try again."
+          
+    chat_history.append({"role": "assistant", "content": message})
+    return jsonify(success=True, message=message)
+
+
     
     # todo map this to line 41 of chatbot-v3. Can this be trimmed down to .list, .value, and then return jsonify?
-    response = client.beta.threads.messages.list(thread_id).data[0]
-
-    text_content = None
-
-    # Iterate through the content objects to find the first text content
-    for content in response.content:
-        if content.type == "text":
-            text_content = content.text.value
-            break  # Exit the loop once the first text content is found
-          
-    log_run(run.status)
+    response = client.beta.threads.messages.list(thread_id).data[0]     
     
-    # Check if text content was found
-    if text_content:
-        chat_history.append({"role": "assistant", "content": text_content})
-        return jsonify(success=True, message=text_content)
-    else:
-        # Handle the case where no text content is found
-        return jsonify(success=False, message="No text content found")
-      
-    
-
+# Reset the chat
 @app.route("/reset", methods=["POST"])
 def reset_chat():
     global chat_history
-    # chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
-    # todo change this also to the message we want to see
     chat_history = [{"role": "system", "content": "Hey there! How can i assist you with your learning today?"}]
 
     global thread_id
@@ -160,8 +152,7 @@ def initialize():
     create_assistant()
     create_thread()
     
-
-
-
+    
+# Run the flask server
 if __name__ == "__main__":
     app.run()
